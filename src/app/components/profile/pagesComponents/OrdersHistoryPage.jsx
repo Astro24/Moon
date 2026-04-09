affe80d279f1c5b886988ec1fe50570b83300891
Mohammed-H-Alsultan <alsultan.mohammedh@gmail.com>
2026-04-09 11:50:26 +0300

    feat(Profile): Add order history page
    - Add OH content to the page
    
    - Change folder profile path from /images/profile to public/profile
    
    - Fix the path in profile page
    
    - Delete PageTranstion.jsx  and stick with the current transtion

--- src/app/components/profile/pagesComponents/OrdersHistoryPage.jsx
+++ src/app/components/profile/pagesComponents/OrdersHistoryPage.jsx
@@ -1,6 +1,9 @@
 "use client";
 import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
 
+
+// TODO: Add functionality to this page
 export default function OrdersHistoryPage() {
   return (
     <section className="overflow-hidden">
@@ -24,8 +27,94 @@ export default function OrdersHistoryPage() {
           transition={{ type: "spring", stiffness: 400, damping: 20 }}
           className="flex flex-col items-center w-full p-10 text-start"
         >
-          <div className="bg-white h-130 w-full rounded-2xl shadow-md">
-            <h1>Orders History</h1>
+          <div className="bg-white w-full rounded-2xl shadow-md">
+            <div className="overflow-x-auto">
+              <table className="table p-4">
+                {/* head */}
+                <thead>
+                  <tr className="bg-base-200 text-white font-bold">
+                    <th>Photo</th>
+                    <td>Product</td>
+                    <td>Price</td>
+                    <td>QuanTity</td>
+                    <td>Subtotal</td>
+                  </tr>
+                </thead>
+                <tbody className="[&_td]:border-b [&_td]:border-gray-300 [&_th]:border-b [&_th]:border-gray-300">
+                  {/* row 1 */}
+                  <tr>
+                    <th>
+                      <Image
+                        src={"/profile/plate.svg"}
+                        width={120}
+                        height={180}
+                        alt="proudect item"
+                      />
+                    </th>
+                    <td>Porcelain Dinner Plate (27cm)</td>
+                    <td>$59</td>
+                    <td>2</td>
+                    <td className="text-[#C69B7B]">$118</td>
+                  </tr>
+                  {/* row 2 */}
+                  <tr>
+                    <th>
+                      <Image
+                        src={"/profile/valo-matte-white-vase.svg"}
+                        width={120}
+                        height={180}
+                        alt="proudect item"
+                      />
+                    </th>
+                    <td>Ophelia Matte Natural Vase</td>
+                    <td>$168</td>
+                    <td>1</td>
+                    <td className="text-[#C69B7B]">$168</td>
+                  </tr>
+                  {/* row 3 */}
+                  <tr>
+                    <th>
+                      <Image
+                        src={"/profile/julo-blue-salad-plate.svg"}
+                        width={120}
+                        height={180}
+                        alt="proudect item"
+                      />
+                    </th>
+                    <td>Porcelain Dinner Plate</td>
+                    <td>$70</td>
+                    <td>1</td>
+                    <td className="text-[#C69B7B]">$70</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="flex justify-center items-center gap-2 mt-6 p-4">
+                <button className="w-9 h-9 flex items-center justify-center rounded-md text-gray-600 hover:bg-gray-100">
+                  {/* alternative ways to do this in jsx is putting &lt; same with right arrow &gt; */}
+                  {"<"}
+                </button>
+
+                <button className="w-9 h-9 flex items-center justify-center bg-[#3A3845] text-white font-bold">
+                  1
+                </button>
+
+                <button className="w-9 h-9 flex items-center justify-center border border-[#3A3845] text-[#3A3845] font-bold hover:bg-gray-100">
+                  2
+                </button>
+
+                <button className="w-9 h-9 flex items-center justify-center text-gray-600 hover:bg-gray-100">
+                  3
+                </button>
+
+                <button className="w-9 h-9 flex items-center justify-center text-gray-600 hover:bg-gray-100">
+                  4
+                </button>
+
+                <button className="w-9 h-9 flex items-center justify-center text-gray-600 hover:bg-gray-100">
+                  {">"}
+                </button>
+              </div>
+            </div>
           </div>
         </motion.div>
       </AnimatePresence>
