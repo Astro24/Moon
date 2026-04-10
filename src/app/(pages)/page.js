bd45137918b6c4d1b9b1b3a31899ce4a72e527b3
Mohammed-H-Alsultan <alsultan.mohammedh@gmail.com>
2026-04-10 16:51:32 +0300

    feat(home): Add categories section in home
    Add cat.jsx file and add all the content on it with animation using inline props to enable animation as whole block, since it used one I just added a var i instead of variant to make the list animated
    
    Add all images needed to /public/home

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/app/components/home/Hero";
+import Cat from "../components/home/Cat";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Cat />
     </main>
   );
 }
