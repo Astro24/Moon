3aa3d8e436833e6d0713d3c20ed77355154bf18a
Mohammed-H-Alsultan <alsultan.mohammedh@gmail.com>
2026-04-10 16:51:32 +0300

    feat(home): Add sales section in home page
    - Add Sales.jsx file to components folder
    - Add content section in Sales.jsx
    - Add animation using whileInView for scroll reveal
    - Add Sales.svg image to /public/home
    - Add Sales.jsx component to page.js (home page)

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/home/Hero";
 import Cat from "../components/home/Cat";
+import Sales from "../components/home/Sales";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Cat />
+      <Sales />
     </main>
   );
 }
