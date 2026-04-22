e89e47764cabe9e375417da33e2d2c80a46f2b56
Mohammed-H-Alsultan <alsultan.mohammedh@gmail.com>
2026-04-22 11:33:23 +0300

    refactor(profile): delete shipping address page - Shipping design is no more in the website that's why I delete it.

--- src/app/(pages)/Profile/shipping-address/page.js
+++
@@ -1,9 +0,0 @@
-import ShippingAddressPage from "@/app/components/profile/pagesComponents/ShippingAddressPage";
-
-export default function shippingAddress() {
-  return (
-    <main className="min-h-screen">
-      <ShippingAddressPage />
-    </main>
-  );
-}
